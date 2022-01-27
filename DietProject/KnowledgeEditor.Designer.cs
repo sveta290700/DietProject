15e8234b79c4792e88193a6c39f68280fc4020fb
sveta290700 <777rsb@mail.ru>
2022-01-27 12:33:36 +1000

    KnowledgeEditor sections list

--- DietProject/KnowledgeEditor.Designer.cs
+++ DietProject/KnowledgeEditor.Designer.cs
@@ -30,19 +30,72 @@ namespace DietProject
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KnowledgeEditor));
+            this.KEChoiceLabel = new System.Windows.Forms.Label();
+            this.KLSectionsListBox = new System.Windows.Forms.ListBox();
+            this.SectionLabel = new System.Windows.Forms.Label();
+            this.KEChoiceButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // KEChoiceLabel
+            // 
+            resources.ApplyResources(this.KEChoiceLabel, "KEChoiceLabel");
+            this.KEChoiceLabel.Name = "KEChoiceLabel";
+            // 
+            // KLSectionsListBox
+            // 
+            resources.ApplyResources(this.KLSectionsListBox, "KLSectionsListBox");
+            this.KLSectionsListBox.FormattingEnabled = true;
+            this.KLSectionsListBox.Items.AddRange(new object[] {
+            resources.GetString("KLSectionsListBox.Items"),
+            resources.GetString("KLSectionsListBox.Items1"),
+            resources.GetString("KLSectionsListBox.Items2"),
+            resources.GetString("KLSectionsListBox.Items3"),
+            resources.GetString("KLSectionsListBox.Items4"),
+            resources.GetString("KLSectionsListBox.Items5"),
+            resources.GetString("KLSectionsListBox.Items6"),
+            resources.GetString("KLSectionsListBox.Items7")});
+            this.KLSectionsListBox.Name = "KLSectionsListBox";
+            this.KLSectionsListBox.TabStop = false;
+            // 
+            // SectionLabel
+            // 
+            resources.ApplyResources(this.SectionLabel, "SectionLabel");
+            this.SectionLabel.BackColor = System.Drawing.Color.LightGreen;
+            this.SectionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SectionLabel.Name = "SectionLabel";
+            // 
+            // KEChoiceButton
+            // 
+            resources.ApplyResources(this.KEChoiceButton, "KEChoiceButton");
+            this.KEChoiceButton.BackColor = System.Drawing.Color.PaleGreen;
+            this.KEChoiceButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.KEChoiceButton.Name = "KEChoiceButton";
+            this.KEChoiceButton.TabStop = false;
+            this.KEChoiceButton.UseVisualStyleBackColor = false;
             // 
             // KnowledgeEditor
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.KEChoiceButton);
+            this.Controls.Add(this.SectionLabel);
+            this.Controls.Add(this.KLSectionsListBox);
+            this.Controls.Add(this.KEChoiceLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "KnowledgeEditor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label KEChoiceLabel;
+        private System.Windows.Forms.ListBox KLSectionsListBox;
+        private System.Windows.Forms.Label SectionLabel;
+        private System.Windows.Forms.Button KEChoiceButton;
     }
 }
