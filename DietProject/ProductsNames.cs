6aa73725cc6adcde9e19ccbdfa895f14832edbfd
sveta290700 <777rsb@mail.ru>
2022-01-28 17:50:38 +1000

    ProductsNames add and delete

--- DietProject/ProductsNames.cs
+++ DietProject/ProductsNames.cs
@@ -6,16 +6,13 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using System.IO;
-using System.Reflection;
 
 namespace DietProject
 {
     public partial class ProductsNames : Form
     {
-        private SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + new DirectoryInfo(Application.StartupPath).Parent.Parent.Parent.FullName + @"\Database.mdf;Integrated Security=True");
         private SqlDataAdapter adapter;
-        private DataTable table = new DataTable();
+        private DataTable ProductsNamesTable = new DataTable();
         public ProductsNames()
         {
             InitializeComponent();
@@ -29,34 +26,64 @@ namespace DietProject
                 ErrorForm.ErrorLabel.Text = "Название продукта не может быть пустым.";
                 ErrorForm.ShowDialog();
             }
+            else
+            {
+                Program.sqlConnection.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO ProductsNames VALUES (N'" + ProductNameTextBox.Text.ToString() + "');", Program.sqlConnection);
+                cmd.ExecuteNonQuery();
+                Program.sqlConnection.Close();
+                ProductsNamesTable = new DataTable();
+                PNProductsNamesListBox.DataSource = ProductsNamesTable;
+                Program.sqlConnection.Open();
+                adapter = new SqlDataAdapter("SELECT * FROM ProductsNames", Program.sqlConnection);
+                adapter.Fill(ProductsNamesTable);
+                PNProductsNamesListBox.DataSource = ProductsNamesTable;
+                PNProductsNamesListBox.DisplayMember = "Name";
+                PNProductsNamesListBox.ValueMember = "Id";
+                Program.sqlConnection.Close();
+            }
         }
 
         private void PNDeleteButton_Click(object sender, EventArgs e)
         {
             {
                 int choice = PNProductsNamesListBox.SelectedIndex;
-                switch (choice)
+                if (choice == -1)
                 {
-                    case -1:
-                        {
-                            ErrorForm ErrorForm = new ErrorForm();
-                            ErrorForm.ErrorLabel.Text = "Выберите название продукта.";
-                            ErrorForm.ShowDialog();
-                            break;
-                        }
-                    default:
-                        break;
+                    ErrorForm ErrorForm = new ErrorForm();
+                    ErrorForm.ErrorLabel.Text = "Выберите название продукта.";
+                    ErrorForm.ShowDialog();
+                }
+                else
+                {
+                    Program.sqlConnection.Open();
+                    DataRowView item = (DataRowView) PNProductsNamesListBox.SelectedItem;
+                    string nameToDelete = item.Row[1].ToString();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM ProductsNames WHERE Name = N'" + nameToDelete + "';", Program.sqlConnection);
+                    cmd.ExecuteNonQuery();
+                    Program.sqlConnection.Close();
+                    ProductsNamesTable = new DataTable();
+                    PNProductsNamesListBox.DataSource = ProductsNamesTable;
+                    Program.sqlConnection.Open();
+                    adapter = new SqlDataAdapter("SELECT * FROM ProductsNames", Program.sqlConnection);
+                    adapter.Fill(ProductsNamesTable);
+                    PNProductsNamesListBox.DataSource = ProductsNamesTable;
+                    PNProductsNamesListBox.DisplayMember = "Name";
+                    PNProductsNamesListBox.ValueMember = "Id";
+                    Program.sqlConnection.Close();
                 }
             }
         }
 
         private void ProductsNames_Load(object sender, EventArgs e)
         {
-            adapter = new SqlDataAdapter("SELECT * FROM ProductsNames", sqlConnection);
-            adapter.Fill(table);
-            PNProductsNamesListBox.DataSource = table;
+            Program.sqlConnection.Open();
+            adapter = new SqlDataAdapter("SELECT * FROM ProductsNames", Program.sqlConnection);
+            adapter.Fill(ProductsNamesTable);
+            PNProductsNamesListBox.DataSource = ProductsNamesTable;
             PNProductsNamesListBox.DisplayMember = "Name";
             PNProductsNamesListBox.ValueMember = "Id";
+            Program.sqlConnection.Close();
         }
     }
 }
--- DietProject/Program.cs
+++ DietProject/Program.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace DietProject
 {
     static class Program
     {
+        public static SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + new DirectoryInfo(Application.StartupPath).Parent.Parent.Parent.FullName + @"\Database.mdf;Integrated Security=True");
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
